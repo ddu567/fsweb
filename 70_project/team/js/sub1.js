ce5fd49ba1e98675e3ffe4d782b170fa16167de3
ddu567 <shbea0503@naver.com>
2023-08-31 17:50:17 +0900

    0831

---
+++ 70_project/team/js/sub1.js
@@ -0,0 +1,8 @@
+// 캐러셀
+$('.bxslider').bxSlider({
+    minSlides: 2,
+    maxSlides: 2,
+    slideWidth: 360,
+    slideMargin: 10
+});
+// https://bxslider.com/examples/carousel-static-number-slides
