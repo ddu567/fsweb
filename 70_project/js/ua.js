ef626414c510c382b177db3d7969c69211d9b38e
ddu567 <shbea0503@naver.com>
2023-09-19 17:20:07 +0900

    0919

--- 70_project/js/ua.js
+++ 70_project/js/ua.js
@@ -1,8 +1,14 @@
-        const ua = navigator.userAgent.toLowerCase();
+/* 
+    https://ddu567.github.io/fsweb/70_project/index.html -> userAgent
+    https://ddu567.github.io/fsweb/70_project/main.html -> 데스크탑
+    https://ddu567.github.io/fsweb/70_project/m/index.html -> 모바일
+*/
 
-        if (ua.indexOf('window')> 0 ){
-            // ua에 window 문자열이 있으면 true -> 데스크탑
-            location.href = 'https://ddu567.github.io/fsweb/70_project/index.html';
-        } else {
-            location.href = 'https://ddu567.github.io/fsweb/70_project/m/index.html';
-        }
+const ua = navigator.userAgent.toLowerCase();
+
+if (ua.indexOf('window') > 0) {
+    // ua에 window 문자열이 있으면 true -> 데스크탑
+    location.href = 'https://ddu567.github.io/fsweb/70_project/index.html';
+} else {
+    location.href = 'https://ddu567.github.io/fsweb/70_project/m/index.html';
+}
