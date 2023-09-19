4df502cf1ac019183d0185fd9b6946b7c962363a
ddu567 <shbea0503@naver.com>
2023-09-19 16:12:02 +0900

    0919

--- 70_project/project4/js/script.js
+++ 70_project/project4/js/script.js
@@ -1,4 +1,22 @@
-// 섹션3: 텍스트 스크롤 모션
+// section2: tabs
+const tabBtn = document.querySelectorAll('#tabs li');
+const tabCont = document.querySelectorAll('#tabs div');
+
+for (let i = 0; i < tabBtn.length; i++) {
+    tabBtn[i].onclick = (e) => {
+        e.preventDefault();
+        // tabs div의 "on" 클래스를 모두 제거
+        for (let j = 0; j < tabBtn.length; j++) {
+            tabCont[j].classList.remove('on');
+        }
+        // tabs div의 "on" 클래스를 버튼 누를 것만 추가
+        tabCont[i].classList.add('on');
+    };
+};
+
+
+
+// section3: text scroll motion
 
 const tit1 = document.querySelector('#sec3 .tit1');
 const tit2 = document.querySelector('#sec3 .tit2');
@@ -23,3 +41,41 @@ window.onscroll = () => {
 
     }
 ;}
+
+
+//section4: video player
+const vidBtn = document.querySelector('.vid .btn');
+const vidBtnSpan = vidBtn.querySelector('span');
+const vidWrap = document.querySelector('.vid');
+const vid =vidWrap.querySelector('video');
+let vidState = 0;
+
+// 비디오 위에 마우스를 올리면
+vidWrap.onmouseenter = () => {
+    // 비디오 플레이 버튼(삼각형)이 보인다.
+    vidBtn.style.display = 'block';
+};
+// 비디오 영역 밖으로 벗어나면
+vidWrap.onmouseleave = () => {
+    // 비디오 플레이 버튼(삼각형)을 숨긴다.
+    vidBtn.style.display = 'none';
+};
+
+// 버튼(play/pause)을 클릭하면
+vidBtn.onclick = () => {
+    // 플레이 되면서
+    if (vidState === 0){
+        // 비디오 실행
+        vid.play();
+        vidState = 1;
+    } else {
+        // 비디오 일시정지
+        vid.pause();
+        vidState = 0;
+    }
+    // span에 on 클래스 제거 비디오 플레이 버튼의 모양이 일시정지 버튼으로 바뀐다.
+    // span에 on 클래스 추가 버튼 모양을 삼각형으로 바꾼다.
+    // toggle은 add와 remove를 번갈아 적용시킨다.
+    vidBtnSpan.classList.toggle('on');
+    
+};
