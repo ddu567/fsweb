dae3def6ddd62d891663939ca6ed922fff71bc3d
ddu567 <shbea0503@naver.com>
2023-10-20 17:48:35 +0900

    1020

---
+++ 02_back/vscjava/Hello/src/a1020/ram/sec01/Calculable.java
@@ -0,0 +1,6 @@
+package a1020.ram.sec01;
+
+public interface Calculable {
+  // 추상 메소드
+  void calculate(int x, int y);
+}
---
+++ 02_back/vscjava/Hello/src/a1020/ram/sec02/exam01/Person.java
@@ -0,0 +1,7 @@
+package a1020.ram.sec02.exam01;
+
+public class Person {
+  public void action(Workable workable) {
+    workable.work();
+  }
+}
