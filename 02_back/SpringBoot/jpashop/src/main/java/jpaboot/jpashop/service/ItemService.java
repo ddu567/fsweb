803c48c2689f124cefb8d4291288a48952a2e4ea
ddu567 <shbea0503@naver.com>
2023-11-15 12:10:28 +0900

    1115

--- 02_back/SpringBoot/jpashop/src/main/java/jpaboot/jpashop/service/ItemService.java
+++ 02_back/SpringBoot/jpashop/src/main/java/jpaboot/jpashop/service/ItemService.java
@@ -29,4 +29,16 @@ public class ItemService {
         return itemRepository.findOne(itemId);
     }
 
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item item = itemRepository.findOne(itemId);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
+    // itemId를 통해 item을 가지고 오면 영속성 객체 item이 불러온다.
+    // 즉, 영속성 객체 자체에 값을 넣기 때문에 변경사항이 알아서 반영된다.
+
+    // 본인이 원하는 값만은 set으로 넣게 되면 @Transactional이 되면서
+    // em.flush가 될 때 자동으로 DB에 반영
 }
